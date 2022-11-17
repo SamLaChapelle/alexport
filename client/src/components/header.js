a12273666821ccee9f8004221063b25a3f436126
Sam LaChapelle <sambosauce@gmail.com>
2022-11-17 15:40:26 -0500

    header compplete & services started

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "../css/header.css";
-import NavBar from "./navbar.js";
 import Logo from "../assets/alexportlogo.png";
 import Paper from "@mui/material/Paper";
 
 function Header() {
   return (
     <div id="header">
-      <NavBar />
       <Paper elevation={3} id="bio">
         {" "}
         <img id="Logo" src={Logo} alt="Esthetics With Alexandra Logo" />
--- client/src/components/services.js
+++ client/src/components/services.js
@@ -1,8 +1,61 @@
 import React from "react";
+import { useState } from "react";
 import "../css/services.css";
+import Paper from "@mui/material/Paper";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const papers = entry.target.querySelector(".papers");
+
+    if (entry.isIntersecting) {
+      papers.classList.add("paperAnimation");
+      return;
+    } else {
+      papers.classList.remove("paperAnimation");
+    }
+  });
+});
+
+const servicePapers = document.querySelectorAll(".servicePapers");
+servicePapers.forEach((servicePaper) => {
+  observer.observe(servicePaper);
+});
 
 function Services() {
-  return <div id="services">Services</div>;
+  return (
+    <div id="services">
+      <div id="serviceTitle">
+        <h2 className="titles">Alexandra LaChapelle</h2>
+        <h4 className="titles" id="subTitle">
+          Master Esthetician
+        </h4>
+      </div>
+      <div className="servicePapers">
+        <Paper
+          className="papers"
+          elevation={3}
+          style={{ backgroundColor: "#f9d5d9" }}
+        ></Paper>{" "}
+      </div>
+      <div className="servicePapers">
+        {" "}
+        <Paper
+          className="papers"
+          id="paperTwo"
+          elevation={3}
+          style={{ backgroundColor: "#f9d5d9" }}
+        ></Paper>{" "}
+      </div>
+
+      <div className="servicePapers">
+        <Paper
+          className="papers"
+          elevation={3}
+          style={{ backgroundColor: "#f9d5d9" }}
+        ></Paper>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
