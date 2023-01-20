ed6da1cd6056086ec9c0196239b0b72e22d8024c
Sam LaChapelle <sambosauce@gmail.com>
2023-01-19 23:40:05 -0500

    about title adjusted

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -8,7 +8,7 @@ function About() {
       <p id="aboutBio">
         Licensed Master Esthetician with a passion for medical grade skincare
         products and treatments! I knew as soon as I started esthetics school
-        that I wanted to work in a med spa. <br />
+        that I wanted to work in a med spa.<br/>
         Being able to offer corrective treatments such as laser and
         microneedling to help people feel more comfortable and confident in
         their skin is what it’s all about!{" "}
